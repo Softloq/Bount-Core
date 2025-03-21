b63a5b1a76b80f7fa0866f3a77481c69c33cb2dd
Brandon Foster <Brandon.Foster4525@outlook.com>
2025-03-21 08:21:55 -0400

    Create Lib-Macro.hpp

---
+++ include/Bount/Core/Lib-Macro.hpp
@@ -0,0 +1,32 @@
+#ifndef BOUNT_CORE_LIB_MACRO_HPP
+#define BOUNT_CORE_LIB_MACRO_HPP
+
+#if defined(_DLL)
+    #define BOUNT_CORE_EXPORT __declspec(dllexport)
+    #define BOUNT_CORE_IMPORT __declspec(dllimport)
+#elif defined(__PIC__) || defined(__pic__)
+    #define BOUNT_CORE_EXPORT __attribute__((visibility("default")))
+    #define BOUNT_CORE_IMPORT
+#endif
+
+#if defined(BOUNT_CORE_BUILD_API)
+    #define BOUNT_CORE_API BOUNT_CORE_EXPORT
+#elif defined(_DLL)
+    #define BOUNT_CORE_API BOUNT_CORE_IMPORT
+#else
+    #define BOUNT_CORE_API
+#endif
+
+#ifndef BOUNT_CORE_EXPORT
+    #define BOUNT_CORE_EXPORT
+#endif
+
+#ifndef BOUNT_CORE_IMPORT
+    #define BOUNT_CORE_IMPORT
+#endif
+
+#ifndef BOUNT_CORE_API
+    #define BOUNT_CORE_API
+#endif
+
+#endif
