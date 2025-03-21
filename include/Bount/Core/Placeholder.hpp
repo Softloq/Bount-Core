05946c061e6b54b0dca0f23653f793df22c5f277
Brandon Foster <Brandon.Foster4525@outlook.com>
2025-03-21 08:22:50 -0400

    add placeholder with case change

---
+++ include/Bount/Core/Placeholder.hpp
@@ -0,0 +1,11 @@
+#ifndef BOUNT_CORE_PLACEHOLDER_HPP
+#define BOUNT_CORE_PLACEHOLDER_HPP
+
+#include "Bount/Core/Lib-Macro.hpp"
+
+namespace Bount::Core
+{
+BOUNT_CORE_API void Placeholder();
+}
+
+#endif
---
+++ src/Bount/Core/Placeholder.cpp
@@ -0,0 +1,8 @@
+#include "Bount/Core/Placeholder.hpp"
+
+namespace Bount::Core
+{
+BOUNT_CORE_API void Core::Placeholder()
+{
+}
+}
